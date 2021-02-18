18e4eb95909086c2dbed959c245edaeeb0087e37
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2021-02-18 17:02:29 -0300

    add cadastro de pessoas no bando de dados

--- sistema/controle.py
+++ sistema/controle.py
@@ -1,13 +1,28 @@
 from PyQt5 import uic,QtWidgets
+import mysql.connector
 
-def main():
+
+#BANCO DE DADOS
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="CHL8xXHp5AfZC8AV",
+    database="desafio_gd"
+)
+
+def cadastrar_pessoa():
     nome = cadastro_pessoas.lineEditNome.text()
     sobrenome = cadastro_pessoas.lineEditSobrenome.text()
-    cpf = cadastro_pessoas.lineEditCPF.text()
 
     print("Nome: ", nome)
     print("Sobrenome: ", sobrenome)
-    print("CPF: ", cpf)
+
+    #SALVANDO DADOS NO BANCO DE DADOS
+    cursor = db.cursor()
+    sql = "INSERT INTO pessoas (nome, sobrenome) VALUES (%s,%s)"
+    data = (str(nome), str(sobrenome))
+    cursor.execute(sql,data)
+    db.commit()
 
 
 
@@ -15,16 +30,19 @@ def call_cadastro_pessoa():
     cadastro_pessoas.show()
 
 
+# insert into pessoas (nome,sobrenome) values (nome,sobrenome);
+
+
 
 app = QtWidgets.QApplication([])
 
 # CARREGA SCREENS
-mainScreen = uic.loadUi("screens/main.ui")
-cadastro_pessoas = uic.loadUi("screens/cadastroPessoas.ui")
+mainScreen = uic.loadUi("sistema/screens/main.ui")
+cadastro_pessoas = uic.loadUi("sistema/screens/cadastroPessoas.ui")
 
 # EVENT LISTENER
 mainScreen.pessoasBtnCadastrar.clicked.connect(call_cadastro_pessoa)
-cadastro_pessoas.btnCadastrar.clicked.connect(main)
+cadastro_pessoas.btnCadastrar.clicked.connect(cadastrar_pessoa)
 
 # MOSTRA MAIN SCREEN
 mainScreen.show()
