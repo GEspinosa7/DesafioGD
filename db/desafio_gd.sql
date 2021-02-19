d27f9cdd3ae8aac0213012cec85b2b6f5b70cb5e
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2021-02-19 17:34:55 -0300

    add tabelas sala e sala de cafe + relacionamento com pessoas

--- db/desafio_gd.sql
+++ db/desafio_gd.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
 --
--- Host: localhost    Database: desafio_gd
+-- Host: localhost    Database: desafio
 -- ------------------------------------------------------
 -- Server version	8.0.23
 
@@ -16,6 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `cafes`
+--
+
+DROP TABLE IF EXISTS `cafes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cafes` (
+  `idCafe` int NOT NULL AUTO_INCREMENT,
+  `numero` int DEFAULT NULL,
+  PRIMARY KEY (`idCafe`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cafes`
+--
+
+LOCK TABLES `cafes` WRITE;
+/*!40000 ALTER TABLE `cafes` DISABLE KEYS */;
+INSERT INTO `cafes` VALUES (1,550),(2,551),(3,552);
+/*!40000 ALTER TABLE `cafes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `pessoas`
 --
 
@@ -23,11 +47,17 @@ DROP TABLE IF EXISTS `pessoas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pessoas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) DEFAULT NULL,
-  `sobrenome` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `idPessoa` int NOT NULL AUTO_INCREMENT,
+  `idSala` int DEFAULT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `sobrenome` varchar(100) DEFAULT NULL,
+  `idCafe` int DEFAULT NULL,
+  PRIMARY KEY (`idPessoa`),
+  KEY `FK_Pessoas_idSala` (`idSala`),
+  KEY `FK_Pessoas_idCafe` (`idCafe`),
+  CONSTRAINT `FK_Pessoas_idCafe` FOREIGN KEY (`idCafe`) REFERENCES `cafes` (`idCafe`),
+  CONSTRAINT `FK_Pessoas_idSala` FOREIGN KEY (`idSala`) REFERENCES `salas` (`idSala`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,16 +66,41 @@ CREATE TABLE `pessoas` (
 
 LOCK TABLES `pessoas` WRITE;
 /*!40000 ALTER TABLE `pessoas` DISABLE KEYS */;
-INSERT INTO `pessoas` VALUES (1,'teste','teste2'),(2,'gabriel','espinosa');
+INSERT INTO `pessoas` VALUES (5,NULL,'teste','asd',NULL),(6,NULL,'teste','sdasdasd',NULL),(7,NULL,'','',NULL),(8,NULL,'','',NULL);
 /*!40000 ALTER TABLE `pessoas` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Dumping events for database 'desafio_gd'
+-- Table structure for table `salas`
+--
+
+DROP TABLE IF EXISTS `salas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `salas` (
+  `idSala` int NOT NULL AUTO_INCREMENT,
+  `numero` int NOT NULL,
+  `capacidade` int DEFAULT NULL,
+  PRIMARY KEY (`idSala`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `salas`
+--
+
+LOCK TABLES `salas` WRITE;
+/*!40000 ALTER TABLE `salas` DISABLE KEYS */;
+INSERT INTO `salas` VALUES (1,445,30),(2,445,30);
+/*!40000 ALTER TABLE `salas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'desafio'
 --
 
 --
--- Dumping routines for database 'desafio_gd'
+-- Dumping routines for database 'desafio'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -57,4 +112,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-18 17:01:33
+-- Dump completed on 2021-02-19 17:34:00
